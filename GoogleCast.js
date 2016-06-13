46e455515d57b73ec159d065bb6e9987203509a3
charliesbox <carlos.lopez@gbox.com>
2016-06-13 15:31:41 -0500

    preparing library

--- GoogleCast.js
+++ GoogleCast.js
@@ -1,2 +1,24 @@
-import { NativeModules } from 'react-native';
-module.exports = NativeModules.GoogleCast;
+import {NativeModules} from 'react-native';
+
+const {GoogleCast} = NativeModules;
+
+export default {
+
+  startScan: function () {
+    GoogleCast.startScan();
+  },
+  getDevices: function () {
+    return GoogleCast.getDevices();
+  },
+  connectToDevice: function (deviceId:string) {
+    GoogleCast.connectToDevice(deviceId);
+  },
+  castMedia: function (mediaUrl:string, title:string, description:string, imageUrl:string) {
+    GoogleCast.castMedia(mediaUrl, title, description, imageUrl);
+  },
+
+  DEVICE_CHANGED: GoogleCast.DEVICE_CHANGED,
+  DEVICE_AVAILABLE: GoogleCast.DEVICE_AVAILABLE,
+  DEVICE_CONNECTED: GoogleCast.DEVICE_CONNECTED,
+
+};
