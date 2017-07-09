6411ae87340794ac1ee488e14a51fe49fd5616ba
Charlie <charlie@wizeline.com>
2017-07-09 15:30:17 -0500

    android fixes

--- android/src/main/java/com/reactlibrary/RNGoogleCastPackage.java
+++ android/src/main/java/com/reactlibrary/RNGoogleCastPackage.java
@@ -1,28 +1,39 @@
-
 package com.reactlibrary;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import android.app.Activity;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
-public class RNGoogleCastPackage implements ReactPackage {
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Charlie on 5/29/16.
+ */
+
+public class GoogleCastPackage implements ReactPackage {
+
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNGoogleCastModule(reactContext));
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new GoogleCastModule(reactContext));
+
+        return modules;
     }
 
     @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
+        return Collections.emptyList();
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+        return Collections.emptyList();
     }
 }
