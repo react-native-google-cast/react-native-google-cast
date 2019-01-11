fe5c1209e074599ed5154d5d008c98eb858768d8
Petr Bela <bela.petr@gmail.com>
2019-01-11 12:12:18 +0100

    Fix compat with RN 0.47

--- android/src/main/java/com/googlecast/GoogleCastPackage.java
+++ android/src/main/java/com/googlecast/GoogleCastPackage.java
@@ -26,7 +26,6 @@ public class GoogleCastPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
