954e47db3899ce66560a9825ef0e453b3055420a
Petr Bela <bela.petr@gmail.com>
2019-04-30 15:06:48 -0600

    [iOS] Fix #75 tintColor

--- ios/RNGoogleCast/RNGoogleCastButtonManager.m
+++ ios/RNGoogleCast/RNGoogleCastButtonManager.m
@@ -1,18 +1,17 @@
-#import <GoogleCast/GoogleCast.h>
 #import <React/RCTViewManager.h>
+#import "RNGoogleCastButton.h"
 
 @interface RNGoogleCastButtonManager : RCTViewManager
 @end
 
 @implementation RNGoogleCastButtonManager
 
-RCT_EXPORT_VIEW_PROPERTY(tintColor, UIColor)
-
 RCT_EXPORT_MODULE()
 
 - (UIView *)view {
-  GCKUICastButton *castButton = [[GCKUICastButton alloc] init];
-  return castButton;
+  return [[RNGoogleCastButton alloc] init];
 }
+
+RCT_EXPORT_VIEW_PROPERTY(tintColor, UIColor)
 
 @end
