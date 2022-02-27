e088b1c2f61118294dc1809a92564d90915e20ae
Petr Bela <bela.petr@gmail.com>
2022-02-27 09:22:27 -0700

    feat (android): expanded controller button #353

--- android/src/main/java/com/reactnative/googlecast/RNGCExpandedControllerActivity.java
+++ android/src/main/java/com/reactnative/googlecast/RNGCExpandedControllerActivity.java
@@ -1,10 +1,6 @@
 package com.reactnative.googlecast;
 
 import android.view.Menu;
-import android.view.MenuItem;
-
-import androidx.core.view.MenuItemCompat;
-import androidx.mediarouter.app.MediaRouteActionProvider;
 
 import com.google.android.gms.cast.framework.CastButtonFactory;
 import com.google.android.gms.cast.framework.media.widget.ExpandedControllerActivity;
@@ -12,16 +8,11 @@ import com.google.android.gms.cast.framework.media.widget.ExpandedControllerActi
 public class RNGCExpandedControllerActivity
   extends ExpandedControllerActivity {
 
-  private static final int MENU_CAST = Menu.FIRST;
-
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     super.onCreateOptionsMenu(menu);
-    menu.add(0, MENU_CAST, Menu.NONE, "Cast");
-    MenuItem item = menu.findItem(MENU_CAST);
-    MenuItemCompat.setActionProvider(
-      item, new MediaRouteActionProvider(getApplicationContext()));
-    CastButtonFactory.setUpMediaRouteButton(this, menu, MENU_CAST);
+    getMenuInflater().inflate(R.menu.cast_expanded_controller_menu, menu);
+    CastButtonFactory.setUpMediaRouteButton(this, menu, R.id.media_route_menu_item);
     return true;
   }
 }
