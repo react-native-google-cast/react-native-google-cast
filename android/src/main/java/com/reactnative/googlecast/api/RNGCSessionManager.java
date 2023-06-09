6c92070f2b71d3c9ce7f74d46b6fc1057f18eb9e
Sufian <9989266+SufianBabri@users.noreply.github.com>
2023-06-09 22:47:13 +0500

    fix: prevent crash when ending session due to missing GMS

--- android/src/main/java/com/reactnative/googlecast/api/RNGCSessionManager.java
+++ android/src/main/java/com/reactnative/googlecast/api/RNGCSessionManager.java
@@ -87,6 +87,11 @@ public class RNGCSessionManager
     getReactApplicationContext().runOnUiQueueThread(new Runnable() {
       @Override
       public void run() {
+        if (!RNGCCastContext.isCastApiAvailable(getReactApplicationContext())) {
+          promise.resolve(null);
+          return;
+        }
+
         SessionManager sessionManager =
           CastContext.getSharedInstance(getReactApplicationContext())
             .getSessionManager();
